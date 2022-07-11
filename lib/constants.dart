a17bff26ea6328cf5026c3265d5e42bf4914d4fa
umer <ausk47@gmail.com>
2022-07-11 11:24:03 +0500

    Add all my files

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const Color kScaffoldColor = Color(0xFFFFFFFF);
+const Color kPrimaryColor = Color(0xFF239B56);
+const Color kSecondaryColor = Color(0xFF28B463);
+const Color kTextPrimaryColor = Color(0xFF3E5481);
+const Color kTextSecondaryColor = Color(0xFF9FA5C0);
+const Color kTextWhiteColor = Color(0xFFFDFEFE);
+const Color kErrorBorderColor = Color(0xFFF94C66);
+const Color kFillColor = Color(0xFFF4F5F7);
+const Color kIconColor = Color(0xFF3E5481);
